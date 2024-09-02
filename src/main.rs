fc19bf2b5c6700c42887ffd9d240d7e094ffd07a
JF Ding <jfding@gmail.com>
2024-09-03 01:13:27 +0800

    add clap args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name= "todor")]
+#[command(version, about= "yet-another TODO cli in Rust", long_about=None)]
+struct Cli {
+    /// working dir
+    #[arg(short, long, value_name = "FOLDER")]
+    dir: Option<String>,
+
+    /// inbox file
+    #[arg(short, long, value_name = "FILE")]
+    inbox: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// -> add todo item to inbox
+    #[clap(visible_alias("a"))]
+    Add,
+    /// -> list all todo items in inbox
+    #[clap(visible_aliases(["l", "ls"]))]
+    List,
+    /// -> edit todo inbox file
+    #[clap(visible_aliases(["e", "ed"]))]
+    Edit,
+    /// -> count items in inbox
+    #[clap(visible_aliases(["c"]))]
+    Count,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("{:?}", args);
+    match args.command {
+        Some(Commands::Add) => {
+            println!("todo add item")
+        }
+        Some(Commands::List) | None => {
+            println!("todo list item")
+        }
+        Some(Commands::Edit) => {
+            println!("todo edit item")
+        }
+        Some(Commands::Count) => {
+            println!("todo count item")
+        }
+    }
 }
