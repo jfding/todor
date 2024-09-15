784dd8c063bca1ac72d4045afa2e29195d39d385
JF Ding <jfding@gmail.com>
2024-09-15 22:47:51 +0800

    more visible checkbox chars and promp for empty box

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,16 @@ fn main() {
         Some(Commands::Mark) | None => {
             let mut todo = TaskBox::new(inbox_path);
             let (tasks,_) = todo._list();
+            if tasks.is_empty() {
+                println!(" {} left!", "nothing".purple());
+                return
+            }
 
             execute!(io::stdout(), BlinkingBlock).expect("failed to set cursor");
 
             let mystyle: RenderConfig = RenderConfig::default()
-                .with_unselected_checkbox("󰄱".into())
-                .with_selected_checkbox("󰄵".into())
+                .with_unselected_checkbox("󰄗".into())
+                .with_selected_checkbox("󰄸".into())
                 .with_highlighted_option_prefix("➡️".into())
                 .with_scroll_up_prefix("↥".into())
                 .with_scroll_down_prefix("↧".into());
@@ -49,13 +53,19 @@ fn main() {
         Some(Commands::List { all }) => {
             let mut todo = TaskBox::new(inbox_path);
             let (tasks, dones) = todo._list();
-            for t in tasks {
-                println!("󰄷 {}", t.bold())
+
+            if tasks.is_empty() {
+                println!(" {} left!", "nothing".purple());
+            } else {
+                for t in tasks {
+                    println!(" 󰄗  {}", t.bold())
+                }
             }
+
             if all {
                 println!("");
                 for t in dones {
-                    println!("󰄹 {}", t.strikethrough())
+                    println!(" 󰄸  {}", t.strikethrough())
                 }
             }
         }
