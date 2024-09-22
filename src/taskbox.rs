a9a8f2337a5cf9579ab6fcb1feb5db706e9b7371
JF Ding <jfding@gmail.com>
2024-09-22 18:02:23 +0800

    tunings for windows target buildings

--- src/taskbox.rs
+++ src/taskbox.rs
@@ -14,10 +14,13 @@ const DATA_BASE : &str = ".local/share/todor";
 const INBOX_NAME :&str  = "INBOX";
 
 pub fn get_inbox_file(dir: Option<String>, inbox: Option<String>) -> PathBuf {
+    // for windows compatibility
+    let rel_base :PathBuf = DATA_BASE.split("/").collect();
+
     let base_path = dir.map(PathBuf::from).unwrap_or_else(|| {
         dirs::home_dir()
             .expect("cannot get home directory")
-            .join(PathBuf::from(DATA_BASE))
+            .join(rel_base)
     });
     fs::create_dir_all(&base_path).expect("Failed to create base directory");
 
